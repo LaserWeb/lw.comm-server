16bb59bf314ee425d5405d38ccc0ecfed33e740e
cprezzi <cprezzi@comsulting.ch>
2017-02-07 17:41:21 +0100

    Changed laserTest to procentual power

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ config.webPort = process.env.WEB_PORT || 8000;
 config.serverVersion = '4.0.12'
 config.apiVersion = '4.0.1';
 
-config.verboseLevel = 1;
+config.verboseLevel = 3;
 config.logLevel = 0;
 
 module.exports = config;
