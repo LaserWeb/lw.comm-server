210dc88d260b11ff9ffab62c944a58205f5f6986
Claudio Prezzi <cprezzi@comsulting.ch>
2017-05-13 17:49:57 +0200

    Version bump

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ require('dotenv').load({ silent: true });
 var config = {};
 
 config.webPort = process.env.WEB_PORT || 8000;
-config.serverVersion = '4.0.68';
-config.apiVersion = '4.0.2';
+config.serverVersion = '4.0.73';
+config.apiVersion = '4.0.3';
 
 config.verboseLevel = process.env.VERBOSE_LEVEL || 1;
 config.logLevel = process.env.LOG_LEVEL || 0;
