e27f2344259bc6f6d0f461d1580fc6d950cb92db
cprezzi <cprezzi@comsulting.ch>
2017-05-24 18:15:16 +0200

    Version bump

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ require('dotenv').load({ silent: true });
 var config = {};
 
 config.webPort = process.env.WEB_PORT || 8000;
-config.serverVersion = '4.0.78';
+config.serverVersion = '4.0.80';
 config.apiVersion = '4.0.3';
 
 config.verboseLevel = process.env.VERBOSE_LEVEL || 1;
 config.logLevel = process.env.LOG_LEVEL || 0;
 
 config.posDecimals = process.env.DRO_DECIMALS || 2;
-config.firmwareWaitTime = process.env.FIRMWARE_WAIT_TIME || 5;
+config.firmwareWaitTime = process.env.FIRMWARE_WAIT_TIME || 10;
 
 module.exports = config;
