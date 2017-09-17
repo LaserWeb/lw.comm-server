42c9fe56ceae6e0919048e9c5350a451847afd25
cprezzi <cprezzi@comsulting.ch>
2017-09-17 22:15:25 +0200

    MPG-TYPE setup via .env file (via config.js)

--- config.js
+++ config.js
@@ -9,11 +9,13 @@ config.apiVersion = '4.0.6';
 config.verboseLevel = process.env.VERBOSE_LEVEL || 1;
 config.logLevel = process.env.LOG_LEVEL || 0;
 config.resetOnConnect = process.env.RESET_ON_CONNECT || 0;
+config.mpgType = process.env.MPG_TYPE || 0;
 
 config.posDecimals = process.env.DRO_DECIMALS || 2;
 config.firmwareWaitTime = process.env.FIRMWARE_WAIT_TIME || 10;
 config.grblWaitTime = process.env.GRBL_WAIT_TIME || 1;
 config.smoothieWaitTime = process.env.SMOOTHIE_WAIT_TIME || 1;
 config.tinygWaitTime = process.env.TINYG_WAIT_TIME || 1;
+
 
 module.exports = config;
