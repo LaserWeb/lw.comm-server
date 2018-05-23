383705ac7669aa20c87ebfc7cfec02cf5248a6a9
Jorge Robles <jorge.diego.robles@gmail.com>
2018-05-23 16:37:26 +0200

    Updating version

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').load({ silent: true });
 var config = {};
 
 config.webPort = process.env.WEB_PORT || 8000;
-config.serverVersion = '4.0.127';
+config.serverVersion = '4.0.130';
 config.apiVersion = '4.0.6';
 
 config.verboseLevel = process.env.VERBOSE_LEVEL || 1;
